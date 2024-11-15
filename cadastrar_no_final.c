595e62459e10d9ca15b9612bc655c73e2c79ad0c
Leonardo260106 <168600954+Leonardo260106@users.noreply.github.com>
2024-11-15 17:49:43 -0300

    cadastrar final

--- cadastrar_no_final.c
+++ cadastrar_no_final.c
@@ -2,34 +2,45 @@
 #include <conio.h>
 
 void CadastrarContaFinal(TipoLista *L) {
+    system("cls");
+    desenhar_tela();
+   
     reg_ContaBanco conta;
     tipoApontador p;
-
-    printf("Digite o código: ");
+    
+    gotoxy(7,7);
+    printf("Codigo: ");
     scanf("%d", &conta.codigo);
-
-    printf("Digite o nome do banco: ");
+    
+    gotoxy(7,8);
+    printf("Nome do banco: ");
     fflush(stdin);
     fgets(conta.Banco, 50, stdin);
     conta.Banco[strcspn(conta.Banco, "\n")] = '\0';
 
+    gotoxy(7,9);
     printf("Digite a agência: ");
     scanf("%d", &conta.agencia);
 
+    gotoxy(7,10);
     printf("Digite o número da conta: ");
     scanf("%d", &conta.numConta);
 
+    gotoxy(7,11);
     printf("Digite o tipo de conta: ");
     fflush(stdin);
     fgets(conta.tipo_conta, 50, stdin);
     conta.tipo_conta[strcspn(conta.tipo_conta, "\n")] = '\0';
 
+    gotoxy(7,12);
     printf("Digite o saldo: ");
     scanf("%f", &conta.saldo);
 
+    gotoxy(7,13);
     printf("Digite o limite: ");
     scanf("%f", &conta.Limite);
-
+    
+    gotoxy(2,23);
     printf("Digite o status da conta (0 - Inativa, 1 - Ativa): ");
     scanf("%d", &conta.statusConta);
 
