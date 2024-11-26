56f3a20bdec4f46caab5cd61187182908f20c3a4
Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
2024-11-26 20:21:29 -0300

    Update remover_conta_final.c

--- remover_conta_final.c
+++ remover_conta_final.c
@@ -1,22 +1,25 @@
 #include "Funcoes.h"
 #include <conio.h>
 
+// Função para remover a última conta cadastrada na lista
 void RemoverContaFinal(TipoLista *L) {
-    tipoApontador p, temp;
-    int confirmacao;
+    tipoApontador p, temp;  // ponteiro para a lista temporario
+    int confirmacao;  
 
-    system("cls");
-    desenhar_tela();
+    system("cls");  
+    desenhar_tela();  
 
+    // Verifica se a lista de contas está vazia
     if (L->Primeiro == NULL) {
-        gotoxy(7, 12);
-        printf("Nenhuma conta cadastrada!");
-        getch();
-        return;
+        gotoxy(7, 12);  
+        printf("Nenhuma conta cadastrada!");  // mostra que não tem contas
+        getch();  
+        return; 
     }
 
-    temp = L->Ultimo;
+    temp = L->Ultimo;  // Aponta para a última conta da lista
 
+    // Mostra as informações se der certo
     gotoxy(7, 7);
     printf("Ultima conta cadastrada:");
     gotoxy(7, 8);
@@ -36,34 +39,40 @@ void RemoverContaFinal(TipoLista *L) {
     gotoxy(7, 15);
     printf("Status: %d", temp->conteudo.statusConta);
 
+    // Solicita confirmação do usuário para remover a conta
     gotoxy(7, 17);
     printf("Deseja remover esta conta? (1 - Sim / 2 - Não): ");
     scanf("%d", &confirmacao);
 
+    // se não for 1 ele cancela 
     if (confirmacao != 1) {
         gotoxy(7, 19);
-        printf("Operação cancelada.");
-        getch();
-        return;
+        printf("Operacao cancelada.");  
+        getch();  
+        return;  
     }
 
+    // se tiver so um, ele remove apenas ele
     if (L->Primeiro == L->Ultimo) {
-        free(L->Primeiro);
-        L->Primeiro = NULL;
-        L->Ultimo = NULL;
+        free(L->Primeiro);  // fuma a memória da última conta
+        L->Primeiro = NULL;  // ele deixa a lista como NULL
+        L->Ultimo = NULL;  // ele deixa a lista como NULL
     } else {
+        // se tiver outro, ele vai ate o penultimo
         p = L->Primeiro;
-        while (p->proximo != L->Ultimo) {
-            p = p->proximo;
+        while (p->proximo != L->Ultimo) {  // Vai ate  o penúltimo elemento
+            p = p->proximo;  // Move o ponteiro para o próximo
         }
-        free(L->Ultimo);
-        L->Ultimo = p;
-        L->Ultimo->proximo = NULL;
+        free(L->Ultimo);  // Fuma a memória da última conta
+        L->Ultimo = p;  // O penúltimo elemento agora é o último
+        L->Ultimo->proximo = NULL;  
     }
 
+    // Fala que removeu
     gotoxy(7, 19);
     printf("Conta removida com sucesso!");
-     gravar_contas(L);
-    getch();
-    menucontasbancarias();
+    
+    gravar_contas(L);  // Grava a lista atualizada no arquivo
+    getch();  
+    menucontasbancarias(); 
 }
